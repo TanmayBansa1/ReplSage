ebfec84097ecec3cec5c8f279e4d1b65adb432f7
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-01-31 23:04:14 +0530

    use-project hook added

---
+++ src/hooks/use-project.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { api } from '~/trpc/react'
+import {useLocalStorage} from 'usehooks-ts'
+const useProject = () => {
+
+    const { data: projects } = api.project.getProjects.useQuery()
+    const [selectedProjectID, setSelectedProjectID] = useLocalStorage("replsage-selected-project", "")
+    const project = projects?.find((project) => project.id === selectedProjectID)
+  return (
+    { projects, selectedProjectID, setSelectedProjectID, project }
+  )
+}
+
+export default useProject
