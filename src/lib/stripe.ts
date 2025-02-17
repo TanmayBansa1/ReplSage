c590cb4eb45e5cca41981fa456550a3f10e85652
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-02-17 18:00:31 +0530

    added stripe lib

---
+++ src/lib/stripe.ts
@@ -0,0 +1,34 @@
+'use server'
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+
+export async function createCheckoutSession(credits: number){
+    const {userId} = await auth()
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+            {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: `${credits} Sage Credits`,
+                    },
+                    unit_amount: Math.round( (credits/50)*100),
+                },
+                quantity: 1,
+            },
+        ],
+        customer_creation: 'always',
+        mode: 'payment',
+        success_url: `${process.env.NEXT_PUBLIC_URL}/dashboard`,
+        cancel_url: `${process.env.NEXT_PUBLIC_URL}/billing`,
+        client_reference_id: userId!.toString(),
+        metadata: {
+            credits: credits
+        }
+    });
+    return redirect(session.url!);
+}
