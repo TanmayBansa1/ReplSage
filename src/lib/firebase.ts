427b23646d2298a659fec98a7c058b623937061b
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-02-18 10:52:02 +0530

    updated firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6emiOkNohRAt165R_ydZj0DvFrK5ik48",
-  authDomain: "replsage-f928b.firebaseapp.com",
-  projectId: "replsage-f928b",
-  storageBucket: "replsage-f928b.firebasestorage.app",
-  messagingSenderId: "1054921066051",
-  appId: "1:1054921066051:web:7608ab429f984b3313b6f6",
-  measurementId: "G-JW0BYK7H4Z"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
