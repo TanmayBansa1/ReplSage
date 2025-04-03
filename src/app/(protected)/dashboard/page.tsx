49c48f739a94b658537faeafb81f734fdf7a67a6
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-04-03 17:07:36 +0530

    updated ux of dashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -43,8 +43,8 @@ const Dashboard = () => {
 
       <div className='mt-4'>
         <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 '>
-          <QuestionCard></QuestionCard>
           <MeetingCard></MeetingCard>
+          <QuestionCard></QuestionCard>
 
         </div>
       </div>
