bea8dcc3b50e3bc094fe60a3e3a0e71ee53ff5fd
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-05-13 13:36:12 +0530

    added bg and init values to authforms

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,13 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <div className='flex justify-center items-center h-screen'>
+  return <div className='flex justify-center items-center h-screen g-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)] '>
+    {/* <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div> */}
 
-  <SignIn />
+  <SignIn 
+  initialValues={{
+    emailAddress: 'test@example.com',
+  }}/>
   </div> 
 }
+
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,11 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <SignUp />
+    <div className='flex justify-center items-center h-screen g-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)] '>
+
+      <SignUp initialValues={{
+        emailAddress: 'test@example.com'
+      }}/>
     </div>
   )
 }
